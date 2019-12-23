7bc2205c6d19351a22c58a4399dd6111ff607a58
David Adegoke <davidadegoke31@gmail.com>
2019-12-23 14:41:05 +0100

    Repeating fixes I already made before :-|

--- src/components/Dsidebar.vue
+++ src/components/Dsidebar.vue
@@ -1,118 +1,214 @@
 <template>
-  <div id="sidebar-nav" class="sidebar">
-    <div
-      class="slimScrollDiv"
-      style="position: relative; overflow: hidden; width: auto; height: 95%;"
-    >
-      <div
-        class="sidebar-scroll"
-        style="overflow: hidden; width: auto; height: 95%;"
-      >
-        <nav>
-          <ul class="nav">
-            <li class="welcome-text">
-              <img
-                src="https://carspot.scriptsbundle.com/wp-content/uploads/2018/04/6.png"
-                class="img-responsive"
-                alt="Profile Picture"
-              />
-              <div class="text-box">Welcome back: <span>testuser </span></div>
-            </li>
-            <li>
-              <router-link to="/maindashboard">
-                <i class="la la-home"></i> <span>Dashboard</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/editProfile">
-                <i class="la la-pencil"></i> <span>Edit Profile</span>
-              </router-link>
-            </li>
-
-            <li>
-              <router-link to="/messaging">
-                <i class="la la-envelope"></i> <span>My Messages</span
-                ><span class="badge referral">0</span>
-              </router-link>
-            </li>
-            <li>
-              <a href="#subPages" class="collapsed" data-toggle="collapse"
-                ><i class="la la-tasks"></i> <span>My Inventory</span>
-                <i class="icon-submenu la la-chevron-left"></i
-              ></a>
-              <div id="subPages" class="collapse ">
-                <ul class="nav">
-                  <li>
-                    <router-link to="/publishedInventory">
-                      <span>Published</span><span class="badge">0</span>
-                    </router-link>
-                  </li>
-                  <li>
-                    <router-link to="/ordersInventory">
-                      <span>Expired</span><span class="badge">0</span>
-                    </router-link>
-                  </li>
-                  <li>
-                    <router-link to="/messaging">
-                      <span>Sold</span><span class="badge">0</span>
-                    </router-link>
-                  </li>
-                  <li>
-                    <router-link to="/messaging">
-                      <span>Pending</span><span class="badge">0</span>
-                    </router-link>
-                  </li>
-                  <li>
-                    <router-link to="/messaging">
-                      <span>Saved</span><span class="badge">0</span>
-                    </router-link>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <router-link to="/refferal">
-                <i class="la la-users"></i><span>Referral</span
-                ><span class="badge referral">0</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/refferal">
-                <i class="la la-star"></i><span>My Ratings</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/orders">
-                <i class="la la-check-circle-o"></i><span>My Orders</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/orders">
-                <i class="la la-cog"></i><span>Settings</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/signin">
-                <i class="fa fa-sign-out"></i><span>Logout</span>
-              </router-link>
-            </li>
-          </ul>
-        </nav>
+  <div>
+    <div class="horizontal-header clearfix" style="z-index: 1001;">
+      <div class="container">
+        <!-- <a id="horizontal-navtoggle" class="animated-arrow"><span></span></a> -->
+        <div
+          id="tx-menu-toggle"
+          class="openbtn closed"
+          style="margin: 0px; position: relative; float: left; display: inline-block; top: 16px; height: 100%;"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <span class="smllogo">
+          <img src="../assets/images/brand/tradexplora-01.png" alt />
+        </span>
+        <!-- <i href="tel:245-6325-3256" class="callusbtn"><i class="fa fa-phone" aria-hidden="true"></i></a> -->
       </div>
+    </div>
+    <div id="tempSidebar" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="#">Home</a>
+      <a href="#">Categories</a>
+      <a href="#">My Profile</a>
+      <a href="#">My Messages</a>
+      <a href="#">Favorites</a>
+      <a href="#">Notifications</a>
+    </div>
+    <div id="sidebar-nav" class="sidebar">
       <div
-        class="slimScrollBar"
-        style="background: rgb(0, 0, 0); width: 7px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 643px;"
-      ></div>
-      <div
-        class="slimScrollRail"
-        style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"
-      ></div>
+        class="slimScrollDiv"
+        style="position: relative; overflow: hidden; width: auto; height: 95%;"
+      >
+        <div class="sidebar-scroll" style="overflow: hidden; width: auto; height: 95%;">
+          <nav>
+            <ul class="nav">
+              <li class="welcome-text">
+                <img
+                  src="https://carspot.scriptsbundle.com/wp-content/uploads/2018/04/6.png"
+                  class="img-responsive"
+                  alt="Profile Picture"
+                />
+                <div class="text-box">
+                  Welcome back:
+                  <span>testuser</span>
+                </div>
+              </li>
+              <li>
+                <router-link to="/maindashboard">
+                  <i class="la la-home"></i>
+                  <span>Dashboard</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/editProfile">
+                  <i class="la la-pencil"></i>
+                  <span>Edit Profile</span>
+                </router-link>
+              </li>
+
+              <li>
+                <router-link to="/messaging">
+                  <i class="la la-envelope"></i>
+                  <span>My Messages</span>
+                  <span class="badge referral">0</span>
+                </router-link>
+              </li>
+              <li>
+                <a href="#subPages" class="collapsed" data-toggle="collapse">
+                  <i class="la la-tasks"></i>
+                  <span>My Inventory</span>
+                  <i class="icon-submenu la la-chevron-left"></i>
+                </a>
+                <div id="subPages" class="collapse">
+                  <ul class="nav">
+                    <li>
+                      <router-link to="/publishedInventory">
+                        <span>Published</span>
+                        <span class="badge">0</span>
+                      </router-link>
+                    </li>
+                    <li>
+                      <router-link to="/ordersInventory">
+                        <span>Expired</span>
+                        <span class="badge">0</span>
+                      </router-link>
+                    </li>
+                    <li>
+                      <router-link to="/messaging">
+                        <span>Sold</span>
+                        <span class="badge">0</span>
+                      </router-link>
+                    </li>
+                    <li>
+                      <router-link to="/messaging">
+                        <span>Pending</span>
+                        <span class="badge">0</span>
+                      </router-link>
+                    </li>
+                    <li>
+                      <router-link to="/messaging">
+                        <span>Saved</span>
+                        <span class="badge">0</span>
+                      </router-link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <router-link to="/refferal">
+                  <i class="la la-users"></i>
+                  <span>Referral</span>
+                  <span class="badge referral">0</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/rating">
+                  <i class="la la-star"></i>
+                  <span>My Ratings</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/ordersInventory">
+                  <i class="la la-check-circle-o"></i>
+                  <span>My Orders</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/mainSettings">
+                  <i class="la la-cog"></i>
+                  <span>Settings</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/signin">
+                  <i class="fa fa-sign-out"></i>
+                  <span>Logout</span>
+                </router-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div
+          class="slimScrollBar"
+          style="background: rgb(0, 0, 0); width: 7px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 643px;"
+        ></div>
+        <div
+          class="slimScrollRail"
+          style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  /* eslint-disable no-undef */
+  methods: {
+    sync() {
+      $(document).ready(function() {
+        $("#tx-menu-toggle").click(function() {
+          $(this).toggleClass("open");
+        });
+
+        let sideBarTrigger = $(".openbtn");
+        let sideBar = $("#sidebar-nav");
+        let dashboardContent = $(".dashboard-main");
+        // let closeBtn = $("")
+        sideBarTrigger.click(() => {
+          if (sideBarTrigger.hasClass("closed")) {
+            //to open
+            sideBar.css({ left: "0px" });
+            dashboardContent.css({
+              left: "250px",
+              transition: "all 0.3s ease-in-out;"
+            });
+            sideBarTrigger.toggleClass("closed");
+            console.log("Sidebar is now open");
+          } else {
+            //to close
+            sideBar.css({ left: "-260px" });
+            dashboardContent.css({
+              left: "0px",
+              transition: "all 0.3s ease-in-out;"
+            });
+            sideBarTrigger.toggleClass("closed");
+            console.log("Sidebar is now closed");
+          }
+        });
+      });
+    }
+  },
+  watch: {
+    $route: "sync"
+  },
+  created() {
+    this.sync();
+  }
+};
 </script>
 
-<style></style>
+<style>
+@media only screen and (max-width: 991px) {
+  .horizontal-header .smllogo {
+    display: inline-block;
+    margin-top: -8px;
+  }
+}
+.sidebar .nav > li > a .badge {
+  margin-bottom: 10px;
+}
+</style>
