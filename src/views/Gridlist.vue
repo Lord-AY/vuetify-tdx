21831a890b2bb40e83117487654e7098f7158871
David Adegoke <davidadegoke31@gmail.com>
2020-01-02 20:53:32 +0100

    fix gridlist mobile logo issue

--- src/views/Gridlist.vue
+++ src/views/Gridlist.vue
@@ -1724,3 +1724,11 @@ export default {
 
 /*@import "../assets/carspot-css/wp-content/themes/carspot/footerSpecial.css";*/
 </style>
+<style >
+@media only screen and (max-width: 991px){
+  .horizontal-header .smllogo{
+    position: relative !important;
+    top: -20px !important;
+  }
+}
+</style>
