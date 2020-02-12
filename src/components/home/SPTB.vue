b3b622b9a823876676f9f0f5ae31bf1817d17285
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2020-02-12 16:56:46 +0100

    updating

--- src/components/home/SPTB.vue
+++ src/components/home/SPTB.vue
@@ -83,7 +83,9 @@ export default {
           draggable: true,
           edgeFriction: 0.30,
           swipe: true,
-          autoplay: true
+          autoplay: true,
+          nextArrow: '<i class="fa fa-arrow-right"></i>',
+          prevArrow: '<i class="fa fa-arrow-left"></i>'
       }
     }
   },
