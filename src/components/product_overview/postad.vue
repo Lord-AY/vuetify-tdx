f531bc16317bc7f9716aa072930a3cb683795dff
Oyinkan Oyetunmibi <oyin-k@macs-MacBook-Pro.local>
2020-02-14 05:27:57 +0100

    other dashboard things

--- src/components/product_overview/postad.vue
+++ src/components/product_overview/postad.vue
@@ -903,70 +903,28 @@ vue/no-parsing-error*/
         <div class="col-lg-4 col-md-12 mobile-hidden">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title" style="font-size: 14px; font-weight: 600;" >Benefits Of Premium Ad</h3>
+              <h3 class="card-title" style="font-size: 14px; font-weight: 600;" >Benefits Of Boosted Ads</h3>
             </div>
             <div class="card-body pb-2">
               <ul class="list-unstyled widget-spec vertical-scroll mb-0">
                 <li>
                   <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium Ads Active
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
                   >Premium ads are displayed on top
                 </li>
                 <li>
                   <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads will be Show in Google results
+                  >Reach a larger audience quickly.
                 </li>
                 <li>
                   <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium Ads Active
+                  >Make quick sales
                 </li>
                 <li>
                   <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads are displayed on top
+                  >Target premium audiences that will ensure purchase
                 </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads will be Show in Google results
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium Ads Active
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads are displayed on top
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads will be Show in Google results
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium Ads Active
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads are displayed on top
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads will be Show in Google results
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium Ads Active
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads are displayed on top
-                </li>
-                <li>
-                  <i class="fa fa-check text-success" aria-hidden="true"></i
-                  >Premium ads will be Show in Google results
-                </li>
+              
+                
               </ul>
             </div>
           </div>
--- src/services/chartDataServices.js
+++ src/services/chartDataServices.js
@@ -4,49 +4,50 @@ export const userChartData = {
       labels: ['january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'],
       datasets: [
         { // one line graph
-          label: 'trades',
+          label: 'Boosted ads',
           data: [0, 0, 1, 2, 12, 7, 15, 14, 11, 12, 2, 4],
           backgroundColor: [
-            'rgba(54,73,93,.5)', // Blue
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
-            'rgba(54,73,93,.5)',
+            '#4CAF50'
           ],
           borderColor: [
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
-            '#36495d',
+        
+            '#4CAF50'
           ],
           borderWidth: 3
         },
         { // another line graph
-          label: 'transactions',
-          data: [4.8, 12.1, 12.7, 6.7, 46.8, 31.4, 50.7, 49.2, 13.0, 12.4, 11.0, 12.0],
+          label: 'Active',
+          data: [3, 9, 40, 27, 68, 3, 50, 49, 1, 12.4, 11, 1],
           backgroundColor: [
-            'rgba(71, 183,132,.5)', // Green
+            '#45AAF1', // Green
           ],
           borderColor: [
-            '#47b784',
+            '#45AAF1',
           ],
           borderWidth: 3
-        }
+        },
+        // {  another line graph
+        //   label: 'Expired',
+        //   data: [4, 12, 1, 6.7, 46.8, 31.4, 50.7, 49.2, 13.0, 12.4, 11.0, 12.0],
+        //   backgroundColor: [
+        //     '#F5365C', // Green
+        //   ],
+        //   borderColor: [
+        //     '#F5365C',
+        //   ],
+        //   borderWidth: 3
+        // },
+        { // another line graph
+          label: 'Sold',
+          data: [1, 10, 19.7, 36.7, 36.8, 32.4, 5.7, 4.2, 13.0, 12.4, 11.0, 12.0],
+          backgroundColor: [
+            '#ECB402', // Green
+          ],
+          borderColor: [
+            '#ECB402',
+          ],
+          borderWidth: 3
+        },
       ]
     },
     options: {
